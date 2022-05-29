0f71c76e25af27bb32215cd5e9eb6545064d7d73
Katheryna Rudenko <rydakatrinr@gmail.com>
2022-05-29 08:19:31 +0300

    add getInfo about EUR

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -4,18 +4,21 @@ import { UsersService } from '../users/users.service';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css'],
-  // imgUrl:'https://www.google.com/search?q=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B0&rlz=1C1SQJL_ukUA951UA953&sxsrf=ALiCzsYA1yPpKrF4HfIoSx34fPu7I-nwig:1653647486722&source=lnms&tbm=isch&sa=X&ved=2ahUKEwie_Z34vP_3AhUGyosKHebDDcIQ_AUoAXoECAEQAw&biw=1536&bih=722&dpr=1.25#imgrc=aCKVDqazvebneM'
+  styleUrls: ['./card.component.css'],  
 })
+
 export class CardComponent implements OnInit {
   base = 'USD';
+  baseSecond = 'EUR';
   cont2 = 'UAH';
   inputV = 1;
   currJson: any = [];
   result: any = '';
+  resultSecond: any = ''
   constructor(private currency: UsersService) {}
   ngOnInit(): void {
     this.getInfo();
+    this.getInfoSecond();
   }
   getInfo() {
     this.currency
@@ -23,7 +26,16 @@ export class CardComponent implements OnInit {
       .subscribe((data) => {
         this.currJson = JSON.stringify(data);
         this.currJson = JSON.parse(this.currJson);
-        this.result = this.currJson.conversion_result;
+        this.resultSecond = this.currJson.conversion_result;
       });
   }
+  getInfoSecond() {
+    this.currency
+      .getData(this.baseSecond, this.cont2, this.inputV)
+      .subscribe((data) => {
+        this.currJson = JSON.stringify(data);
+        this.currJson = JSON.parse(this.currJson);
+        this.result = this.currJson.conversion_result;
+      });
+    }
 }
